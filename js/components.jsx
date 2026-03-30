4455d081e96919b3a1bfa1aa132cf04b1efe9a28
LCRN <70516001+LucasCRENAIS@users.noreply.github.com>
2026-03-30 11:50:12 +0200

    Add React UI components extracted from App component

---
+++ js/components.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ComponentA = () => {
+    return <div>Component A</div>;
+};
+
+export const ComponentB = () => {
+    return <div>Component B</div>;
+};
+
+// Add more components here...
